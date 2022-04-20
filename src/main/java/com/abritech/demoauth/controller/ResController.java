b331605278f656c226aa9249ba7a4703d466f1d4
yilikalkebede <45570788+yilikalkebede@users.noreply.github.com>
2022-04-19 21:42:23 -0400

    first commit

---
+++ src/main/java/com/abritech/demoauth/controller/ResController.java
@@ -0,0 +1,27 @@
+package com.abritech.demoauth.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurant")
+public class ResController {
+
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcome(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Welcome to our restaurant");
+    }
+
+    @GetMapping("/token")
+    public Jwt token(@AuthenticationPrincipal Jwt jwtToken){
+        return jwtToken;
+    }
+
+
+}
